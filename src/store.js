9c6a15ebc08ed4f52c021b7bc69f679bedc2aadd
chaekeun <chaen328@khu.ac.kr>
2023-12-17 02:33:14 +0900

    1217 feat : redux등등 셋팅

--- src/store.js
+++ src/store.js
@@ -1,29 +1,19 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-let cart = createSlice({
-  name: "cart",
-  initialState: [
-    { id: 0, name: "White and Black", count: 2 },
-    { id: 2, name: "Grey Yordan", count: 1 },
-  ],
-  reducers: {
-    addCount(state, action) {
-      let number = state.findIndex((a) => {
-        return a.id === action.payload;
-      });
-      state[number].count++;
-    },
-    addItem(state, action) {
-      state.push(action.payload);
-    },
-  },
+let conversation = createSlice({
+  name: "conversation",
+  initialState: [{ role: "assistant", content: "질문을 던지는 assistant" }],
+  // reducers: {
+  //   addItem(state, action) {
+  //     state.push(action.payload);
+  //   },
+  // },
 });
 
 export default configureStore({
   reducer: {
-    // user: user.reducer,
-    cart: cart.reducer,
+    conversation: conversation.reducer,
   },
 });
 
-export let { addCount, addItem } = cart.actions;
+// export let { addCount, addItem } = cart.actions;
